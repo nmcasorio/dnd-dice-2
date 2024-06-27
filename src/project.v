0f24b4dfb412aa0ce3b67f2ff2d46d7a9b360a52
nmcasorio <nmcasorio@gmail.com>
2024-06-27 09:56:17 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,9 +17,8 @@ module tt_um_lsfr (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = {4'b0000, random_number}; // Output the random number on the lower 4 bits, upper 4 bits are 0
+  assign uio_oe  = assign uio_oe  = 8'b11111111;              // Enable all uio_out bits as outputs
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
